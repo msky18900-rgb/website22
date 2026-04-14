7212c5a64b0cce15e7b782c73c2a4c7e2bcc2e3c
msky18900-rgb <msky18900@gmail.com>
2026-04-14 17:38:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website22/', // Important: This must be your repository name
+  base: './', // Using relative paths is the safest way to avoid blank screens
 })
