890c8dbe0181f505b8028d779ea73426d2dc7fe7
msky18900-rgb <msky18900@gmail.com>
2026-04-14 17:55:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'website22' with your EXACT repo name on GitHub
+  // Use the name of your GitHub repository here
   base: '/website22/', 
 })
